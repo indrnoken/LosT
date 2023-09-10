1981b1e852c3b5b905b6a031719a31b744777797
indrnoken <indrnoken@gmail.com>
2023-09-10 13:29:41 +0700

    mengatur fungsi toggle dari hamberger menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -19,4 +24,13 @@ const navMenu = document.querySelector("#nav-menu");
 hamberger.addEventListener("click", function () {
   hamberger.classList.toggle("hamberger-active");
   navMenu.classList.toggle("hidden");
+});
+
+//klik diluar hamburger
+
+window.addEventListener("click", function (e) {
+  if (e.target != hamberger && e.target != navMenu) {
+    hamberger.classList.remove("hamberger-active");
+    navMenu.classList.add("hidden");
+  }
 });
