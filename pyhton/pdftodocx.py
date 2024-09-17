0497bdf428f8785e8a0fb7d01b9b31e841287fcb
indrnoken <indrnoken@gmail.com>
2024-09-17 19:26:54 +0900

    foto tulisan

---
+++ pyhton/pdftodocx.py
@@ -0,0 +1,5 @@
+from pdftodocx import converter
+pdf_file = 'pyhton\22.96.3297_M. Dikri Nugraha_Tugas 2 Pengantar Sosiologi.pdf'
+docx_file = 'pyhton\22.22.96.3297_M. Dikri Nugraha_Tugas 2 Pengantar Sosiologi.docx'
+cv = Converter(docx_file)
+cv.close()
